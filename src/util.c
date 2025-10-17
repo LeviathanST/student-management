f3f0e8cd0195458487eb0e37f081d16f7df8a37f
LeviathanST <phamquochung170505@gmail.com>
2025-10-17 19:37:49 +0700

    util: end print with '\n'

--- src/util.c
+++ src/util.c
@@ -7,18 +7,19 @@ void print_err(const char *format, ...) {
   va_start(args, format);
   printf("ERROR: ");
   vprintf(format, args);
+  printf("\n");
   va_end(args);
 }
 
 const char *app_strerror(int errno) {
   switch (errno) {
   case 1001:
-    return "the name length > the max name length\n";
+    return "the name length > the max name length";
   case 1002:
-    return "the id length > the max id length\n";
+    return "the id length > the max id length";
   case 1003:
-    return "the major length > the max major length\n";
+    return "the major length > the max major length";
   default:
-    return "Unknown error\n";
+    return "Unknown error";
   }
 }
