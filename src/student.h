06375910aedcb876a58e2f75a5149d4b8b0cc4ed
LeviathanST <phamquochung170505@gmail.com>
2025-10-21 10:59:02 +0700

    student: add search and sort

--- src/student.h
+++ src/student.h
@@ -1,6 +1,5 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-#endif
 
 #define ID_MAX_LENGTH 15
 #define NAME_MAX_LENGTH 50
@@ -21,9 +20,9 @@ void student_new(Student *s, const char *id, const char *name, int birth_year,
 /// Get a student by ID from the file.
 Student student_get(char id[15]);
 
-/// Get a student by ID from the file.
+/// Get all students from the file.
 /// @return a student array.
-Student *student_getAll(Student buf[]);
+Student *student_getAll();
 
 /// Write a student to the file.
 void student_add(Student s);
@@ -32,3 +31,17 @@ void student_add(Student s);
 /// Assign `NULL` if that field don't need to update.
 void student_update(Student s, const char *name, const int *birth_year,
                     const char *major);
+
+enum SortType { inc, desc };
+
+/// @return a sorted student array .
+Student *student_getAllAndSort(enum SortType type);
+
+enum SearchBy { ident, name };
+
+/// Search a student `by` name or ID.
+/// @return a student.
+/// @return `NULL` value if the student not found.
+Student *student_search(enum SearchBy by, const char *value);
+
+#endif
