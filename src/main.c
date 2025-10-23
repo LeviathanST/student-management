e9c78e3593f40984f45f95b92fd528df014573a1
LeviathanST <phamquochung170505@gmail.com>
2025-10-23 20:44:49 +0700

    main: default UI

--- src/main.c
+++ src/main.c
@@ -1,18 +1,32 @@
-#include "student.h"
 #include "util.h"
+
 #include <errno.h>
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-  Student s;
-  student_new(&s, "hehe", "test", 2005, "SE");
+  int exit = 0;
+  char user_input[20];
+
+  while (exit == 0) {
+    printf("SM> ");
+    scanf("%s", user_input);
+
+    if (strcmp("hi", user_input) == 0) {
+      printf("Hi there!\n");
+    } else if (strcmp("exit", user_input) == 0) {
+      exit = 1;
+      printf("Bye!\n");
+    } else {
+      print_err("Command not found!");
+    }
+  }
 
   if (errno > 0) {
+    // Catch the error and print the error message
     print_err(app_strerror(errno));
     return 0;
   }
 
-  printf("ID %s\n", s.id);
-  printf("name %s\n", s.name);
   return 0;
 }
